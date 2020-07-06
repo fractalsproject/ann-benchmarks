c7adea7a56e4bf239a89d8cfca8258f37e7a4458
Briggs599 <bdriggs@ucsd.edu>
2020-07-06 23:08:19 +0000

    more changes

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-PYTHONPATH=. python3 run.py --dataset base0-1-merge --algorithm faiss-ivf --runs 1 --local > command 1>&2 | tee output.txt
+#!/bin/bash
+PYTHONPATH=. python3 run.py --dataset base0-1-merge --algorithm faiss-ivf --definitions algosDeep.yaml --runs 1 --local 2>&1 | tee out_new.txt
